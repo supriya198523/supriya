eaa118245dde0b821fa0d3a94f15e9bbd0e8f0af
supriya198523 <supriya111522@gmail.com>
2024-08-09 22:44:43 +0100

    2nd commit

--- demo.java
+++ demo.java
@@ -5,6 +5,9 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi Supriya in ireland");
+		System.out.println("I love u pankaj");
+		System.out.println("I love shlok");
+		
 		
 	}
 
